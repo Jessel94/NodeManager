3e893d877331d498adcafd3fd9bd9d2e300e6aa0
Jessel94 <jesselakerveld@hotmail.com>
2016-05-16 20:46:10 +0200

    Updated NodeManager

--- NodeManagerClean/Controllers/ContainersController.cs
+++ NodeManagerClean/Controllers/ContainersController.cs
@@ -1,4 +1,5 @@
 ﻿using NodeManagerClean.Models;
+using NodeManagerClean.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,26 +11,41 @@ namespace NodeManagerClean.Controllers
 {
     public class ContainersController : ApiController
     {
-        Container[] containers = new Container[]
-        {
-            new Container { Id = 1, Name = "Dock 1", QueueId = 1, LastChecked = DateTime.Today.AddDays(-1) },
-            new Container { Id = 2, Name = "Swarm stuffs", QueueId = 45, LastChecked = DateTime.Today },
-            new Container { Id = 3, Name = "FBI Survailence container", QueueId = 80, LastChecked = DateTime.Today.AddHours(-1) }
-        };
 
-        public IEnumerable<Container> GetAllContainers()
+        private ContainerRepository containerRepository;
+
+        public ContainersController()
         {
-            return containers;
+            this.containerRepository = new ContainerRepository();
+        }
+
+        public Container[] Get()
+        {
+            return containerRepository.GetAllContainers();
         }
 
         public IHttpActionResult GetContainer(int id)
         {
+            var containers = containerRepository.GetAllContainers();
             var container = containers.FirstOrDefault((p) => p.Id == id);
             if (container == null)
             {
                 return NotFound();
             }
             return Ok(container);
+        }
+
+        public HttpResponseMessage Post(Container container)
+        {
+            container.Id = 4;
+            container.QueueId = 90;
+            container.LastChecked = DateTime.Today;
+
+            this.containerRepository.SaveContainer(container);
+
+            var response = Request.CreateResponse<Container>(System.Net.HttpStatusCode.Created, container);
+
+            return response;
         }
     }
 }
---
+++ NodeManagerClean/Services/ContainerRepository.cs
@@ -0,0 +1,96 @@
+﻿using NodeManagerClean.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NodeManagerClean.Services
+{
+    public class ContainerRepository
+    {
+        private const string CacheKey = "ContainerStore";
+
+        public ContainerRepository()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                if (ctx.Cache[CacheKey] == null)
+                {
+                    var Containers = new Container[]
+                    {
+                        new Container
+                        {
+                            Id = 1,
+                            Name = "Dock 1",
+                            QueueId = 1,
+                            LastChecked = DateTime.Today.AddDays(-1)
+                        },
+                        new Container
+                        {
+                            Id = 2,
+                            Name = "Swarm stuffs",
+                            QueueId = 45,
+                            LastChecked = DateTime.Today
+                        },
+                        new Container
+                        {
+                            Id = 3,
+                            Name = "FBI Survailence container",
+                            QueueId = 80,
+                            LastChecked = DateTime.Today.AddHours(-1)
+                        }
+                    };
+                    ctx.Cache[CacheKey] = Containers;
+                }
+            }
+        }
+
+
+        public Container[] GetAllContainers()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                return (Container[])ctx.Cache[CacheKey];
+            }
+
+            return new Container[]
+                {
+                    new Container
+                    {
+                        Id = 0,
+                        Name = "Placeholder",
+                        QueueId = 0,
+                        LastChecked = DateTime.Today
+                    }
+                };
+        }
+
+        public bool SaveContainer(Container container)
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                try
+                {
+                    var currentData = ((Container[])ctx.Cache[CacheKey]).ToList();
+                    currentData.Add(container);
+                    ctx.Cache[CacheKey] = currentData.ToArray();
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    }
+}
